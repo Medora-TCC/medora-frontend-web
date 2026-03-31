4a796baf5cb3c4e8232803cafbe5696434c4c016
gustavogogola91 <gustavogogola3@gmail.com>
2026-03-31 13:27:22 -0300

    feat: projeto web (Home) criado e configurado conforme os outros

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Teste, ThemeProvider } from '@medora_web/shared'
+
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <Teste/>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
