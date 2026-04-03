5b4205fd5b2ef1824cbe525258ffd651c8387f36
João Class <88053866+JoaoPauloClass@users.noreply.github.com>
2026-04-03 12:02:41 -0300

    Install Lucid-react

--- apps/web/src/pages/TelaLogin/TelaLogin.tsx
+++ apps/web/src/pages/TelaLogin/TelaLogin.tsx
@@ -1,8 +1,14 @@
+import { Input } from "@medora_web/shared";
+
 export default function TelaLogin(){
     return(
         <div className="w-full flex justify-center items-center h-full ">
-            <div className="bg-surface-raised ">
-                <h1 className="w-1/2 text-primary">LOGIN</h1>
+            <div className="bg-surface w-1/2">
+                <h1 className="text-primary text-3xl">Login</h1>
+                <div className="w-50">
+                    <Input label="Email"/>
+                    <Input label="Senha"/>
+                </div>
             </div>
         </div>
     )
