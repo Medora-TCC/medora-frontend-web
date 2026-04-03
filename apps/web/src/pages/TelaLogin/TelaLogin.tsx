0523e7845a7f8db5f6f38e2519d4056dfde33dc7
João Class <88053866+JoaoPauloClass@users.noreply.github.com>
2026-04-03 10:33:47 -0300

    Criação da page de login

---
+++ apps/web/src/pages/TelaLogin/TelaLogin.tsx
@@ -0,0 +1,9 @@
+export default function TelaLogin(){
+    return(
+        <div className="w-full flex justify-center items-center h-full ">
+            <div className="bg-surface-raised ">
+                <h1 className="w-1/2 text-primary">LOGIN</h1>
+            </div>
+        </div>
+    )
+}
