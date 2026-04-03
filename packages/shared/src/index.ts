b8d5558772d79b7fc2a2e8a6cde9849e20fa6fc1
GustavoSacoman <gustavo.sacoman23@gmail.com>
2026-04-03 02:05:42 -0300

    feat: adicionado os exports para funcionar o input e botoes

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/Teste";
 export * from "./theme/ThemeContext";
 export * from "./theme/themeColors";
+export * from "./components/Input";
+export * from "./components/Button";
