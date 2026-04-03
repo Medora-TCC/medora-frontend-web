f7dfe2e90f8d58b052e32f8ffb2e94b179d58987
GustavoSacoman <gustavo.sacoman23@gmail.com>
2026-04-03 02:02:36 -0300

    feat: alterado app para testar input e botao

--- apps/medora-medico/src/App.tsx
+++ apps/medora-medico/src/App.tsx
@@ -1,12 +1,18 @@
-import { Teste, ThemeProvider } from '@medora_web/shared'
+import {Button, Input, Teste, ThemeProvider } from '@medora_web/shared'
 
 function App() {
   return (
-    <>
-      <ThemeProvider>
-        <Teste/>
-      </ThemeProvider>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-4">
+      <h1 className="text-2xl font-bold">Teste do Design System</h1>
+
+      {/* 2. Use o componente Button com os estados que criamos! */}
+      <Button>Botão Normal</Button>
+      
+      <Button isLoading={true}>Botão Carregando</Button>
+      
+      <Button disabled={true}>Botão Desativado</Button>
+      <Input label="Nome" id="nome" />
+    </div>
   )
 }
 
