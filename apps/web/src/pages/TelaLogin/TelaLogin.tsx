c86425fda98645295816f090dc12d592598fe7e9
João Class <88053866+JoaoPauloClass@users.noreply.github.com>
2026-04-03 13:43:16 -0300

    Install PrimeReact

--- apps/web/src/pages/TelaLogin/TelaLogin.tsx
+++ apps/web/src/pages/TelaLogin/TelaLogin.tsx
@@ -1,15 +1,26 @@
-import { Input } from "@medora_web/shared";
+import { useState } from "react";
+import { Password } from 'primereact/password';
 
-export default function TelaLogin(){
-    return(
+export default function TelaLogin() {
+    const [value, setValue] = useState('');
+    return (
         <div className="w-full flex justify-center items-center h-full ">
             <div className="bg-surface w-1/2">
                 <h1 className="text-primary text-3xl">Login</h1>
                 <div className="w-50">
-                    <Input label="Email"/>
-                    <Input label="Senha"/>
+                    <div className="card flex flex-col justify-content-center">
+                        <label htmlFor="password">Password</label>
+                        <Password
+                            inputId="password" 
+                            value={value} 
+                            onChange={(e) => setValue(e.target.value)} 
+                            feedback={false} 
+                            toggleMask />
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
