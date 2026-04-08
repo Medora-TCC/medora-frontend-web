59388f0292526a051d9685529f4d72bb81a733fd
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-08 01:05:07 -0300

    feat: adicionado componente de Layout para padronização de configurações

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,5 @@
-export * from "./components/Teste";
 export * from "./theme/ThemeContext";
+export * from "./components/Teste";
 export * from "./components/Input";
 export * from "./components/Button";
+export * from "./components/Layout";
