32750522dfb7c5b0d49ec49d4b4b5325b6ebe08b
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-12 16:06:16 -0300

    fix: alterada importação e pastas na página do medico

--- apps/medora-medico/src/App.tsx
+++ apps/medora-medico/src/App.tsx
@@ -1,18 +1,15 @@
-import {Button, Input, Teste, ThemeProvider } from '@medora_web/shared'
+import {Layout, ThemeProvider } from '@medora_web/shared'
+import LoginScreen from './pages/LoginScreen/LoginScreen'
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-4">
-      <h1 className="text-2xl font-bold">Teste do Design System</h1>
-
-      {/* 2. Use o componente Button com os estados que criamos! */}
-      <Button>Botão Normal</Button>
-      
-      <Button isLoading={true}>Botão Carregando</Button>
-      
-      <Button disabled={true}>Botão Desativado</Button>
-      <Input label="Nome" id="nome" />
-    </div>
+    <>
+      <ThemeProvider>
+        <Layout>
+          <LoginScreen/>
+        </Layout>
+      </ThemeProvider>
+    </>
   )
 }
 
