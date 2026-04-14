1f2a2aec679f1ebe811c3df0d4f4b1296192d999
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-14 00:22:44 -0300

    feat: adicionado configuração básica para rotas

--- apps/medora-admin/src/App.tsx
+++ apps/medora-admin/src/App.tsx
@@ -1,12 +1,19 @@
-import { Teste, ThemeProvider} from '@medora_web/shared'
+import { Layout, NotFound, ThemeProvider } from '@medora_web/shared'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 function App() {
   return (
     <>
       <ThemeProvider>
-          <Teste />
+        <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<h1></h1>} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </Layout>
       </ThemeProvider>
-
     </>
   )
 }
