857bd6c0b9e8762e0a16acabdb5f185e9e6382f4
gustavogogola91 <gustavogogola3@gmail.com>
2026-04-27 20:45:05 -0300

    feat: adicionado página de termos de uso

--- apps/medora-medico/src/App.tsx
+++ apps/medora-medico/src/App.tsx
@@ -1,14 +1,16 @@
-import { ThemeProvider, Layout, NotFound } from '@medora_web/shared'
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { ThemeProvider, Layout, NotFound } from "@medora_web/shared";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { TermsOfUsePage } from "./pages/TermsOfUsePage/TermsOfUsePage";
 
 function App() {
   return (
     <>
-    <ThemeProvider>
+      <ThemeProvider>
         <Layout>
           <BrowserRouter>
             <Routes>
-              <Route path="/"/>
+              <Route path="/" />
+              <Route path="termos-de-uso" element={<TermsOfUsePage />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
