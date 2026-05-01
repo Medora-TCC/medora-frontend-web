8f72491cc49b2adf01c4ffa8537546f006190404
Gustavo Sacoman <Gustavo.sacoman23@outlook.com>
2026-04-30 21:47:31 -0300

    feat: alterado app do médico para funcionar com o  novo layout

--- apps/medora-medico/src/App.tsx
+++ apps/medora-medico/src/App.tsx
@@ -1,22 +1,27 @@
-import { ThemeProvider, Layout, NotFound } from "@medora_web/shared";
+import { ThemeProvider, Layout, NotFound, HomePage, AuthLayout } from "@medora_web/shared";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { TermsOfUsePage } from "./pages/TermsOfUsePage/TermsOfUsePage";
 import { PrivacyPolicyPage } from "./pages/PrivacyPolicyPage/PrivacyPolicyPage";
+import LoginScreen from "./pages/LoginScreen/LoginScreen";
+import {RegisterPage} from "./pages/RegisterPage/RegisterPage";
+
 
 function App() {
   return (
     <>
       <ThemeProvider>
-        <Layout>
           <BrowserRouter>
-            <Routes>
-              <Route path="/" />
-              <Route path="termos-de-uso" element={<TermsOfUsePage />} />
-              <Route path="politica-de-privacidade" element={<PrivacyPolicyPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+          <Routes>
+          <Route element={<Layout />}>
+            <Route path="politica-de-privacidade" element={<PrivacyPolicyPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/cadastro" element={<RegisterPage />} />
+            <Route path="termos-de-uso" element={<TermsOfUsePage />} />
+            <Route path="/login" element={<LoginScreen />} />
+          </Route>
+        </Routes>
           </BrowserRouter>
-        </Layout>
       </ThemeProvider>
     </>
   );
