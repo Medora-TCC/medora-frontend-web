fec868d1bac9da9020bd5e5bde717491f32198ba
gustavogogola91 <gustavogogola3@gmail.com>
2026-06-19 14:45:21 -0300

    Adicionado modal de carregamento e modal de de retorno sucesso/erro

--- apps/medora-medico/src/pages/SignaturePage/SignaturePage.tsx
+++ apps/medora-medico/src/pages/SignaturePage/SignaturePage.tsx
@@ -2,30 +2,34 @@ import { lazy, Suspense, useState } from "react";
 import { DocsList } from "./DocsList";
 import { Modal, Spinner } from "@heroui/react";
 import { ModalConfirmacao } from "@medora_web/shared";
+import { CircleCheck, CircleX } from "lucide-react";
 
-const PDFViewer = lazy(() => import('./PDFViewer'));
+const PDFViewer = lazy(() => import("./PDFViewer"));
 
 const MOCK_DOCS = [
   {
-    id: '1',
-    type: 'Prescrição médica',
-    status: 'pending' as const,
-    patient: 'Joãozinho da Silva',
-    date: '14/06/2026',
-    path: "/TesteReceita.pdf"
+    id: "1",
+    type: "Prescrição médica",
+    status: "pending" as const,
+    patient: "Joãozinho da Silva",
+    date: "14/06/2026",
+    path: "/TesteReceita.pdf",
   },
 ];
 
 export function SignaturePage() {
-
   const [isLoading, setIsLoading] = useState(false);
-  const [documentoSelecionado, setDocumentoSelecionado] = useState<string | null>(null);
+  const [isSucesso, setIsSucesso] = useState<boolean | null>(null);
+  const [documentoSelecionado, setDocumentoSelecionado] = useState<
+    string | null
+  >(null);
 
   const assinarDocumento = async () => {
     setIsLoading(true);
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     setIsLoading(false);
-  }
+    setIsSucesso(true);
+  };
 
   return (
     <div className="flex flex-col h-screen overflow-hidden">
@@ -37,7 +41,10 @@ export function SignaturePage() {
 
       <div className="flex flex-1 overflow-hidden">
         <aside className="w-[40%] shrink-0 overflow-y-auto border-r border-divider p-4">
-          <DocsList docs={MOCK_DOCS} setDocumentoSelecionado={setDocumentoSelecionado} />
+          <DocsList
+            docs={MOCK_DOCS}
+            setDocumentoSelecionado={setDocumentoSelecionado}
+          />
         </aside>
 
         <div className="flex flex-1 flex-col overflow-hidden min-w-0">
@@ -56,7 +63,7 @@ export function SignaturePage() {
           <div className="shrink-0 flex justify-end px-8 py-4 border-t border-divider">
             <ModalConfirmacao
               onConfirm={assinarDocumento}
-              disabled={false}
+              disabled={isLoading}
               texto="Deseja assinar o documento?"
               textoBotao="Assinar"
             />
@@ -74,6 +81,36 @@ export function SignaturePage() {
                   <Modal.Body className="flex flex-col items-center justify-center gap-3 py-8">
                     <Spinner />
                     <span>Assinando documento</span>
+                  </Modal.Body>
+                </Modal.Dialog>
+              </Modal.Container>
+            </Modal.Backdrop>
+          </Modal>
+          <Modal>
+            <Modal.Backdrop
+              isOpen={isSucesso != null}
+              onOpenChange={(isOpen) => {
+                if (!isOpen) {
+                  setIsSucesso(null);
+                }
+              }}
+              variant="opaque"
+            >
+              <Modal.Container>
+                <Modal.Dialog aria-label={`${isSucesso ? "Sucesso" : "Erro"}`}>
+                  <Modal.CloseTrigger />
+                  <Modal.Body className="flex flex-col items-center justify-center gap-3 py-8">
+                    {isSucesso ? (
+                      <div className="text-success font-semibold text-lg text-center items-center flex flex-col gap-4">
+                        <CircleCheck size={56} className="mx-auto" />
+                        <span>Documento assinado com sucesso</span>
+                      </div>
+                    ) : (
+                      <div className="text-danger font-semibold text-lg text-center items-center flex flex-col gap-4">
+                        <CircleX size={56} className="mx-auto" />
+                        <span>Erro ao assinar o documento</span>
+                      </div>
+                    )}
                   </Modal.Body>
                 </Modal.Dialog>
               </Modal.Container>
