e1f8b439ad5fa3a90ce5fafdb90804669bd4b273
Gustavo Sacoman <Gustavo.sacoman23@outlook.com>
2026-06-20 14:20:06 -0300

    feat: feito o managerUsers para geranciar os usuários

--- apps/medora-admin/src/App.tsx
+++ apps/medora-admin/src/App.tsx
@@ -1,14 +1,13 @@
 import { Layout, NotFound, ThemeProvider } from '@medora_web/shared'
 import { BrowserRouter, Route, Routes } from 'react-router'
-
+import UserManagementPage from './pages/ManageUsers/ManageUsers'
 function App() {
   return (
     <>
       <ThemeProvider>
         <BrowserRouter>
           <Routes>
-            <Route element={<Layout />}></Route>
-            <Route path="/" />
+            <Route path="/manage-users" element={<UserManagementPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
